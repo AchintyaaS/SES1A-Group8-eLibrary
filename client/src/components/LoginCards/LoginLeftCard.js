e69af5e94ca0422d15054da27256fa78aeeeb41a
IncinerateZ <inferious77@gmail.com>
2021-05-11 11:19:54 +0700

    logo changes

--- client/src/components/LoginCards/LoginLeftCard.js
+++ client/src/components/LoginCards/LoginLeftCard.js
@@ -1,41 +1,43 @@
-import logo from "../../img/utslogo.png";
+import logo from '../../img/elib-logo.svg';
 
 function LoginLeftCard() {
 	return (
 		<div
-			className="login-left-card"
+			className='login-left-card'
 			style={{
-				width: "40vw",
-				height: "66vh",
-				backgroundColor: "rgba(33, 55, 69, 1)",
-				display: "block",
-				margin: "auto",
-				float: "left",
-				marginLeft: "20vw",
-				marginTop: "16vh",
-				borderRadius: "45px",
-				boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+				width: '40vw',
+				height: '66vh',
+				backgroundColor: 'rgba(33, 55, 69, 1)',
+				display: 'block',
+				margin: 'auto',
+				float: 'left',
+				marginLeft: '20vw',
+				marginTop: '16vh',
+				borderRadius: '45px',
+				boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
 			}}
 		>
 			<img
-				className="unselectable"
+				className='unselectable'
 				style={{
-					width: "80%",
-					transform: "translateX(-3%)",
-					marginTop: "1vh",
+					width: '30%',
+					transform: 'translateX(-3%)',
+					marginTop: '15vh',
+					marginLeft: '9vw',
 				}}
 				src={logo}
-			></img>
+			/>
 			<div
-				className="login-title"
+				className='login-title'
 				style={{
-					width: "fit-content",
-					height: "fit-content",
-					color: "rgba(255, 255, 255, 1)",
-					fontFamily: "Roboto Mono",
-					fontSize: "3vw",
-					margin: "auto",
-					transform: "translate(-5vw, -5vh)",
+					width: 'fit-content',
+					height: 'fit-content',
+					color: 'rgba(255, 255, 255, 1)',
+					fontFamily: 'Roboto Mono',
+					fontSize: '3vw',
+					margin: 'auto',
+					marginTop: '7vh',
+					transform: 'translate(-5vw, -5vh)',
 				}}
 			>
 				Library System
